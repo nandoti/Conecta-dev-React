1a35df6405727345275f91c8e50c392a40625953
Fernando Tavares <nando.frontend@gmail.com>
2021-08-11 21:21:28 -0300

    :construction: Criado novo componente

---
+++ src/components/PostCard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { makeStyles } from "@material-ui/core/styles";
+
+
+function PostCard(post) {
+    return (
+        <h1>Post</h1>
+    )
+}
+
+export default PostCard;
